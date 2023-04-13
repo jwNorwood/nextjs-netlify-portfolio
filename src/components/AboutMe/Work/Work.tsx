48a76eb842cedf5821cae3d066846cebe5386dec
Jacob Norwood <jacobnorwood92@gmail.com>
2023-04-12 19:54:25 -0500

    Update Work history

--- src/components/AboutMe/Work/Work.tsx
+++ src/components/AboutMe/Work/Work.tsx
@@ -7,17 +7,20 @@ const Work = () => {
     <>
       <h2>Work History</h2>
       <div className="work">
-        <Company name="Boom Solutions" industry="Construction" years={1} skills={["React", "React Native", "SQLite", "Jest"]}>
-          <p>Worked on a project managment web application for the construction industry. </p>
-          <p>Additonally worked on a native mobile app to aid in time tracking and on site project management. </p>
+        <Company name="Boom Solutions" industry="Construction" years={2} skills={["React", "React Native", "SQLite", "Jest"]}>
+          <p>Used ReactJS and Bootstrap to build out a project management web application for the construction industry.</p>
+          <p>Developed components for an internal Component Library using yalc and rollup.js to share React components across multiple web applications.</p>
+          <p>Used React Native to build out a mobile (iOS and Android) application used for time tracking and onsite project management.</p>
         </Company>
         <Company name="Volly" industry="Finance / Housing" years={2} skills={["React", "Angular", "NodeJS", "Express", "PostgreSQL", "Mocha"]}>
-          <p>Built landing pages and emails as part of marketing campaigns.</p>
-          <p>Worked on adding functionallity to an existing email marketing platform using Angular and Node.js and worked on a client portal using React.</p>
+          <p>Developed new features on web applications using ReactJS, AngularJS, and NodeJS.</p>
+          <p>Created new endpoints and improved functionality and security of the back end of web applications using ExpressJS and NodeJS.</p>
+          <p>Troubleshooted and fixed bugs, security, and accessibility issues.</p>
+          <p>Built out marketing websites, landing pages and forms using HTML, CSS and Javascript.</p>
         </Company>
         <Company name="Columbia Daily Tribune" industry="News" years={2} skills={["Angular", "jQuery"]}>
-          <p>Worked on developing custom features for a CMS on the core news website.</p>
-          <p>Worked on custom marketing sites and pages for events and advertising partners.</p>
+          <p>Built columbiatribune.com and marketing websites using HTML, CSS, Javascript, WordPress and other Content Management Systems.</p>
+          <p>Designed wireframes and created prototypes for new features and new designs.</p>
         </Company>
       </div>
       <style jsx>{`
