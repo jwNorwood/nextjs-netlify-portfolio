f1b60854926de1e3ef2093db0438a07fc4fd2f53
Jacob Norwood <jacobnorwood92@gmail.com>
2023-04-28 20:41:10 -0500

    Build out work history section

--- src/components/AboutMe/Work/Work.tsx
+++ src/components/AboutMe/Work/Work.tsx
@@ -9,18 +9,24 @@ const Work = () => {
       <div className="work">
         <Company name="Boom Solutions" industry="Construction" years={2} skills={["React", "React Native", "SQLite", "Jest"]}>
           <p>Used ReactJS and Bootstrap to build out a project management web application for the construction industry.</p>
+          <p>Used ReactJS to build out a bidding portal for contract work in the construction industry.</p>
           <p>Developed components for an internal Component Library using yalc and rollup.js to share React components across multiple web applications.</p>
           <p>Used React Native to build out a mobile (iOS and Android) application used for time tracking and onsite project management.</p>
+          <p>Used Expo to take advantage of Native mobile apis including location tracking and photos.</p>
+          <p>Published an pushed updates to the application on the Google Play and Apple App store.</p>
         </Company>
         <Company name="Volly" industry="Finance / Housing" years={2} skills={["React", "Angular", "NodeJS", "Express", "PostgreSQL", "Mocha"]}>
-          <p>Developed new features on web applications using ReactJS, AngularJS, and NodeJS.</p>
-          <p>Created new endpoints and improved functionality and security of the back end of web applications using ExpressJS and NodeJS.</p>
-          <p>Troubleshooted and fixed bugs, security, and accessibility issues.</p>
+          <p>Developed new features on a Marketing Automation (email) web applications using ReactJS, AngularJS, and NodeJS.</p>
+          <p>Created new endpoints, troubleshooted and fixed bugs, and improved functionality and security of the back end of web applications using ExpressJS and NodeJS.</p>
+          <p>Created and maintained PostgreSQL databases and tables.</p>
+          <p>Wrote unit tests using Mocha.</p>
+          <p>Worked on new features for a cross product platform in NextJS.</p>
           <p>Built out marketing websites, landing pages and forms using HTML, CSS and Javascript.</p>
         </Company>
-        <Company name="Columbia Daily Tribune" industry="News" years={2} skills={["Angular", "jQuery"]}>
-          <p>Built columbiatribune.com and marketing websites using HTML, CSS, Javascript, WordPress and other Content Management Systems.</p>
-          <p>Designed wireframes and created prototypes for new features and new designs.</p>
+        <Company name="Columbia Daily Tribune" industry="News" years={2} skills={["Angular", "jQuery", "Bootstrap"]}>
+          <p>Built a redesign for columbiatribune.com and built marketing websites using HTML, CSS, Javascript, WordPress and other Content Management Systems.</p>
+          <p>Integrated custom advertising and google ads into the columbiatribune.com</p>
+          <p>Designed wireframes and created prototypes for new features and new designs for the columbiatribune.com and other websites.</p>
         </Company>
       </div>
       <style jsx>{`
